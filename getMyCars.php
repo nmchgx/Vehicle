31af729088b1997cf109ffbfdd937fa53f674060
YannAdams <574380444@qq.com>
2016-04-14 10:41:24 +0800

    geng xin getMyCars

--- getMyCars.php
+++ getMyCars.php
@@ -16,9 +16,9 @@
 	$carmsg[4]['key'] = "车身级别";
 	$carmsg[4]["value"] = "四门四座";
 	$carmsg[5]['key'] = "里程数";
-	$carmsg[5]["value"] = 10000;
+	$carmsg[5]["value"] = "10000 km";
 	$carmsg[6]['key'] = "油量%";
-	$carmsg[6]["value"] = 80;
+	$carmsg[6]["value"] = "90%";
 	$carmsg[7]['key'] = "发动机状态";
 	$carmsg[7]["value"] = '正常';
 	$carmsg[8]['key'] = "变速器状态";
@@ -35,9 +35,9 @@
 	
 	
 	$carmsg[0]['key'] = '汽车品牌';
-	$carmsg[0]['value'] = '宝马';
+	$carmsg[0]['value'] = '雷克萨斯';
 	$carmsg[1]['key'] = "型号";
-	$carmsg[1]["value"] = "A4L";
+	$carmsg[1]["value"] = "ES";
 	$carmsg[2]['key'] = "车牌号";
 	$carmsg[2]["value"] = "京E20849";
 	$carmsg[3]['key'] =  "发动机号";
@@ -45,9 +45,9 @@
 	$carmsg[4]['key'] = "车身级别";
 	$carmsg[4]["value"] = "四门四座";
 	$carmsg[5]['key'] = "里程数";
-	$carmsg[5]["value"] = 10000;
-	$carmsg[6]['key'] = "油量%";
-	$carmsg[6]["value"] = 80;
+	$carmsg[5]["value"] = "10000 km";
+	$carmsg[6]['key'] = "油量";
+	$carmsg[6]["value"] = "90%";
 	$carmsg[7]['key'] = "发动机状态";
 	$carmsg[7]["value"] = '正常';
 	$carmsg[8]['key'] = "变速器状态";
@@ -56,9 +56,9 @@
 	$carmsg[9]["value"] = '正常';
 	
 	$arr['carMsg']['carmsg'] = $carmsg;
-	$arr['carMsg']['Title'] = '宝马A4L';
+	$arr['carMsg']['Title'] = '雷克萨斯ES';
 	$arr['carMsg']['ID'] = 2;
-	$arr['carMsg']['Logo'] = "http://7xst41.com2.z0.glb.clouddn.com/BMW.png";
+	$arr['carMsg']['Logo'] = "http://7xst41.com2.z0.glb.clouddn.com/Lexus.png";
 	
 	$arr1[1] = $arr['carMsg'];
 
