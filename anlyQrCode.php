d9b94069ec8a608abede608b809428b34a8cd14f
YannAdams <574380444@qq.com>
2016-04-21 22:30:52 +0800

    update QrCode

--- anlyQrCode.php
+++ anlyQrCode.php
@@ -60,13 +60,13 @@
 					$carid = $row['Car_ID'];
 					$title = $row['Car_Brand'].$row['Car_Model'];
 					$carLicensePlate = $row['Car_LicensePlate'];
-					$carngineNum = $row['Car_EngineNum'];
+					$carEngineNum = $row['Car_EngineNum'];
 				}
 				
 				$obj['Car_ID'] = $carid;
 				$obj['Car_Title'] = $title;
 				$obj['Car_LicensePlate'] = $carLicensePlate;
-				$obj['Car_EngineNum'] = $carngineNum;
+				$obj['Car_EngineNum'] = $carEngineNum;
 						
 				$array['data'] = $obj;
 				$array['msg'] = "添加车辆成功";
