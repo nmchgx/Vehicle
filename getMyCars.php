8165e49e0b039e71b8f809cbe9c5e469b0ba1981
YannAdams <574380444@qq.com>
2016-04-25 14:21:56 +0800

    test

--- getMyCars.php
+++ getMyCars.php
@@ -5,6 +5,7 @@
 	$str2 = '{"ID":"3","Brand":"宝马","Logo":"宝马","Model":"A4L"，"License":"京E20849","EngNum":"491Q-E649815","BodyLevel":"四门四座","Mil":"10000","GasVol":"80","EngOK":"1","TranOK":"1","LightOK":"1"}';
 	
 	
+	
 	$carmsg[0]['key'] = '汽车品牌';
 	$carmsg[0]['value'] = '宝马';
 	$carmsg[1]['key'] = "型号";
