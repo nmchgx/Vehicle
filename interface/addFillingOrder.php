fbfa84bde20af9e790235deba6c6af66adf5c8e5
nmchgx <nmchgx@163.com>
2016-05-08 21:15:09 +0800

    edit order QRCode

--- interface/addFillingOrder.php
+++ interface/addFillingOrder.php
@@ -19,26 +19,14 @@ $Filling_Station_ID = $_POST['Filling_Station_ID'];
 $Filling_Station = $_POST['Filling_Station'];
 $Filling_Time = $_POST['Filling_Time'];
 
-$data = null;
-$qrcode = null;
-
-
 // 二维码生成
-$data['C_ID'] = $C_ID;
-$data['Car_ID'] = $Car_ID;
-$data['Filling_Type'] = $Filling_Type;
-$data['Filling_Amount'] = $Filling_Amount;
-$data['Filling_Station_ID'] = $Filling_Station_ID;
-$data['Filling_Station'] = $Filling_Station;
-$data['Filling_Time'] = $Filling_Time;
-
-$qrcode['data'] = $data;
-$qrcode_json = JSON($qrcode);
 $timestamp = strtotime('now');
-$qrcode_name = "qrcode-filling-$C_ID-$Car_ID-$timestamp.png";
+$Order_Key = "$C_ID-$Car_ID-$timestamp";
+$qrcode_name = "qrcode-filling-$Order_Key.png";
 $Order_QRCode = "http://7xst41.com2.z0.glb.clouddn.com/$qrcode_name";
+$qrcode_str = "http://139.129.27.123:8010/Vehicle/H5/myOrder.html?Order_Key=$Order_Key";
 
-QRcode::png($qrcode_json, $qrcode_name, QR_ECLEVEL_L, 5, 0);
+QRcode::png($qrcode_str, $qrcode_name, QR_ECLEVEL_L, 5, 0);
 
 // 七牛上传
 chdir(dirname(__FILE__));
@@ -48,7 +36,7 @@ QiniuUpload($qrcode_name);
 // 数据库操作
 $result = null;
 
-$sql="INSERT INTO orders (C_ID,Car_ID,Filling_Type,Filling_Amount,Filling_Total,Filling_Station_ID,Filling_Station,Filling_Time,Order_Status,Order_QRCode) VALUES ('$C_ID','$Car_ID','$Filling_Type','$Filling_Amount','$Filling_Total','$Filling_Station_ID','$Filling_Station','$Filling_Time','1','$Order_QRCode')";
+$sql="INSERT INTO orders (C_ID,Car_ID,Filling_Type,Filling_Amount,Filling_Total,Filling_Station_ID,Filling_Station,Filling_Time,Order_Status,Order_QRCode,Order_Key) VALUES ('$C_ID','$Car_ID','$Filling_Type','$Filling_Amount','$Filling_Total','$Filling_Station_ID','$Filling_Station','$Filling_Time','1','$Order_QRCode','$Order_Key')";
 $sqlResult=$mysql->query($sql);
 
 if(!empty($sqlResult)) {
