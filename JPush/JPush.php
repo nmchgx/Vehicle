aa6a4d384a23dbb38c5da6d20e41b22ae8af5c93
YannAdams <574380444@qq.com>
2016-05-17 17:54:57 +0800

    update

--- JPush/JPush.php
+++ JPush/JPush.php
@@ -1,17 +1,10 @@
 <?php
-chdir(dirname(__FILE__));
-require "../JPush/core/PushPayload.php";
-require "../JPush/core/ReportPayload.php";
-require "../JPush/core/DevicePayload.php";
-require "../JPush/core/SchedulePayload.php";
-require "../JPush/core/JPushException.php";
-/*
+
 require_once("core/PushPayload.php");
 require_once("core/ReportPayload.php");
 require_once("core/DevicePayload.php");
 require_once("core/SchedulePayload.php");
 require_once("core/JPushException.php");
-*/
 
 class JPush {
     const DISABLE_SOUND = "_disable_Sound";
