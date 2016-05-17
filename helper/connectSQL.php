08359c8817350d47e8b1570af916286fb7f1ff99
YannAdams <574380444@qq.com>
2016-05-17 17:44:07 +0800

    update code

--- helper/connectSQL.php
+++ helper/connectSQL.php
@@ -2,6 +2,6 @@
 	header("Content-type: text/html;charset=utf-8;");
 	error_reporting(0); 
 	
-	$conn=mysql_connect("localhost","root","123456");
+	$conn=mysql_connect("localhost","root","");
 	mysql_select_db("vehicle");
 	mysql_query("set names utf8");
