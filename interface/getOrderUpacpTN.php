74bcb292fbd2efa671badfe309183d43e374db13
nmchgx <nmchgx@163.com>
2016-07-16 15:12:20 +0800

    fix bug

--- interface/getOrderUpacpTN.php
+++ interface/getOrderUpacpTN.php
@@ -34,14 +34,12 @@ $params = array(
     'accessType' => '0',		          //接入类型
     'currencyCode' => '156',	          //交易币种，境内商户固定156
 
-    //TODO 以下信息需要填写
+    // 以下信息需要填写
     'merId' => $merId,		//商户代码，请改自己的测试商户号，此处默认取demo演示页面传递的参数
     'orderId' => $orderId,	//商户订单号，8-32位数字字母，不能含“-”或“_”，此处默认取demo演示页面传递的参数，可以自行定制规则
     'txnTime' => $txnTime,	//订单发送时间，格式为YYYYMMDDhhmmss，取北京时间，此处默认取demo演示页面传递的参数
     'txnAmt' => $txnAmt,	//交易金额，单位分，此处默认取demo演示页面传递的参数
 // 		'reqReserved' =>'透传信息',        //请求方保留域，透传字段，查询、通知、对账文件中均会原样出现，如有需要请启用并修改自己希望透传的数据
-
-    //TODO 其他特殊用法请查看 pages/api_05_app/special_use_purchase.php
 );
 
 AcpService::sign ( $params ); // 签名
