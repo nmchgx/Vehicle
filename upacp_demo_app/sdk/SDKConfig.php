e02df70358c0649927469eecc495b841f9a4ab78
nmchgx <nmchgx@163.com>
2016-07-16 09:56:47 +0800

    fix dir

--- upacp_demo_app/sdk/SDKConfig.php
+++ upacp_demo_app/sdk/SDKConfig.php
@@ -2,16 +2,16 @@
  
 // ######(以下配置为PM环境：入网测试环境用，生产环境配置见文档说明)#######
 // 签名证书路径
-const SDK_SIGN_CERT_PATH = 'D:/certs/acp_test_sign.pfx';
+const SDK_SIGN_CERT_PATH = 'C:/acp/certs/acp_test_sign.pfx';
 
 // 签名证书密码
 const SDK_SIGN_CERT_PWD = '000000';
 
 // 密码加密证书（这条一般用不到的请随便配）
-const SDK_ENCRYPT_CERT_PATH = 'D:/certs/acp_test_enc.cer';
+const SDK_ENCRYPT_CERT_PATH = 'C:/acp/certs/acp_test_enc.cer';
 
 // 验签证书路径（请配到文件夹，不要配到具体文件）
-const SDK_VERIFY_CERT_DIR = 'D:/certs/';
+const SDK_VERIFY_CERT_DIR = 'C:/acp/certs/';
 
 // 前台请求地址
 const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
@@ -41,10 +41,10 @@ const SDK_FRONT_NOTIFY_URL = 'http://localhost:8085/upacp_sdk_php/demo/api_05_ap
 const SDK_BACK_NOTIFY_URL = 'http://222.222.222.222/upacp_sdk_php/demo/api_05_app/BackReceive.php';
 
 //文件下载目录 
-const SDK_FILE_DOWN_PATH = 'D:/file/';
+const SDK_FILE_DOWN_PATH = 'C:/acp/file/';
 
 //日志 目录 
-const SDK_LOG_FILE_PATH = 'D:/logs/';
+const SDK_LOG_FILE_PATH = 'C:/acp/logs/';
 
 //日志级别，关掉的话改PhpLog::OFF
 const SDK_LOG_LEVEL = PhpLog::DEBUG;
