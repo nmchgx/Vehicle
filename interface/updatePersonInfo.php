1871e871aefc1363c00a1a40afb2719f8ce06489
YannAdams <574380444@qq.com>
2016-07-17 16:56:03 +0800

    update personinfo

--- interface/updatePersonInfo.php
+++ interface/updatePersonInfo.php
@@ -16,20 +16,9 @@ $C_Sign = $_POST['C_Sign'];
 // 数据库操作
 $result = null;
 
-$sql = "update customer set ";
-if($C_NickName.length > 0){
-	$sql += " C_NickName='".$C_NickName."' ";
-}else if($C_Sex.length>0){
-	$sql += " , C_Sex='".$C_Sex."' ";
-}else if($C_Birthday.length>0){
-	$sql += " , C_Birthday='".$C_Birthday."' ";
-}else if($C_Place.length>0){
-	$sql += " , C_Place='".$C_Place."' ";
-}else if($C_Sign.length>0){
-	$sql += " , C_Sign='".$C_Sign."' ";
-}
+$sql = "update customer set C_NickName='".$C_NickName."', C_Sex='".$C_Sex."', C_Birthday='".$C_Birthday."', C_Place='".$C_Place."',  C_Sign='".$C_Sign."' where C_ID=".$C_ID;
 
-$sql += " where C_ID=".$C_ID;
+$sqlResult=$mysql->query($sql);
 
 if(!empty($sqlResult)) {
     returnData("修改成功", 1);
